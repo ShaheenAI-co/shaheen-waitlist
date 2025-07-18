fed6d8d0a12eb09b12b77903d7befd2a25f9bc48
Ahmad <ahmedgeger@gmail.com>
2025-07-18 21:34:45 +0300

    added mobile responsivty

--- waitlist/src/components/MainHead.jsx
+++ waitlist/src/components/MainHead.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const MainHead = () => {
   return (
-    <div className="  flex flex-col items-center justify-center">
+    <div className="   flex flex-col items-center justify-center">
       <h1 className="text-3xl md:text-5xl lg:text-7xl satoshi-bold text-white mb-6 leading-tight">
-        Say goodbye to social media stress  let{" "}
+        Say goodbye to social media stress <br />  let{" "}
         <span className="italic gradText ">ShaheenAI </span> handle it all
       </h1>
 
